477a55fb04c44f97b8a6583758422b512d7dffc5
Renee Owusu-Poku <rowusupoku@smith.edu>
2022-09-18 19:33:13 -0400

    commit 2

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -36,28 +36,20 @@ class ResaleShop:
         computer_id = buy(computer)
         print("Done.\n")
 
-    def buyComputer(self, ) -> str:
-        # Make sure it worked by checking inventory
-        print("Checking inventory...")
-        print_inventory()
-        print("Done.\n")
+    def sellComputer(self, ) -> str:
+        # Now, let's sell it!
+        print("Selling Item ID:", computer_id)
+        sell(computer_id)
 
+    def refurbish(self, ) -> str:
         # Now, let's refurbish it
         new_OS = "MacOS Monterey"
         print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
         print("Updating inventory...")
         refurbish(computer_id, new_OS)
         print("Done.\n")
-
-        # Make sure it worked by checking inventory
-        print("Checking inventory...")
-        print_inventory()
-        print("Done.\n")
         
-        # Now, let's sell it!
-        print("Selling Item ID:", computer_id)
-        sell(computer_id)
-        
+    def checkInventory(self, ) -> str:
         # Make sure it worked by checking inventory
         print("Checking inventory...")
         print_inventory()
